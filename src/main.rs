88a724767d5a7ef913ce5aee50e972df290e4090
Richo Healey <richo@psych0tik.net>
2015-07-24 14:57:49 -0700

    Serve statics with an override

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 extern crate iron;
+extern crate mount;
+extern crate staticfile;
+
+use std::path::Path;
 
 use iron::prelude::*;
 use iron::status;
+use mount::Mount;
+use staticfile::Static;
+
+fn hello_world(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Hello World!")))
+}
 
 fn main() {
-    fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello World!")))
-    }
+    let mut mount = Mount::new();
 
-    Iron::new(hello_world).http("localhost:3000").unwrap();
+    mount.mount("/", Static::new(Path::new("static")));
+    mount.mount("/hello", hello_world);
+
+    Iron::new(mount).http("localhost:3000").unwrap();
 }
